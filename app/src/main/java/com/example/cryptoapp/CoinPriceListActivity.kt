417291abcaf6066c6550fede933a3a22f18c4a69
vsened <vasily.senederov@gmail.com>
2023-04-02 22:42:22 +0500

    Add adapter for coin price list.

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.adapters.CoinInfoAdapter
+import com.example.cryptoapp.pojo.CoinPriceInfo
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
+    private lateinit var rvCoinPriceInfo: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
+        rvCoinPriceInfo = findViewById(R.id.rvCoinPriceList)
+        val adapter = CoinInfoAdapter()
+        rvCoinPriceInfo.adapter = adapter
+        adapter.onCoinItemClickListener = object: CoinInfoAdapter.OnCoinItemClickListener {
+            override fun onCoinItemClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("CoinPriceListActivity", "Click on $coinPriceInfo")
+            }
+        }
         viewModel = ViewModelProviders.of(this)[CoinViewModel::class.java]
         viewModel.priceList.observe(this) {
-            Log.d("CoinViewModel", "LiveData = ${it.toString()}")
-        }
-        viewModel.getDetailsFor("BTC").observe(this){
-            Log.d("CoinViewModel", "Success in activity = ${it.toString()}")
+            adapter.items = it
         }
     }
 }
---
+++ app/src/main/java/com/example/cryptoapp/adapters/CoinInfoAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.cryptoapp.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.R
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import com.squareup.picasso.Picasso
+
+class CoinInfoAdapter : RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+    var items: List<CoinPriceInfo> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+    var onCoinItemClickListener: OnCoinItemClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_coin_info,
+            parent,
+            false
+        )
+        return CoinInfoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
+        val item = items[position]
+        with(holder) {
+            with(item) {
+                tvSymbols.text = String.format(
+                    itemView.context.getString(R.string.card_label),
+                    fromSymbol,
+                    toSymbol
+                )
+                tvPrice.text = price.toString()
+                tvTimeOfLastUpdate.text = String.format(
+                    itemView.context.getString(R.string.last_update_label),
+                    getFormattedTime()
+                )
+                Picasso.get().load(getFullImageUrl()).into(ivLogoCoin)
+                itemView.setOnClickListener {
+                    onCoinItemClickListener?.onCoinItemClick(this)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    inner class CoinInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivLogoCoin: ImageView = itemView.findViewById(R.id.ivLogoCoin)
+        val tvSymbols: TextView = itemView.findViewById(R.id.tvSymbols)
+        val tvPrice: TextView = itemView.findViewById(R.id.tvPrice)
+        val tvTimeOfLastUpdate: TextView = itemView.findViewById(R.id.tvTimeOfLastUpdate)
+    }
+
+    interface OnCoinItemClickListener {
+        fun onCoinItemClick(coinPriceInfo: CoinPriceInfo)
+    }
+}
